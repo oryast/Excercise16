0d70f9b2e4481b173048069c00013951ddd6237c
oryast <oryastru@gmail.com>
2017-07-26 02:12:15 +0300

    Update encryption.py

--- encryption/encryption.py
+++ encryption/encryption.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 class Encryption(object):
     def encrypt(self, data_to_encrypt):
         raise NotImplementedError("Not implemented!")
 
     def decrypt(self, data_to_decrypt):
         raise NotImplementedError("Not implemented!")
-=======
-
->>>>>>> 7cd66b3eae274e71f964aef0412cbeb6ffb7c438
