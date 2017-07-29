6777d2f1915c53652011c56c2e22c758681f63f9
Or <oryastru@gmail.com>
2017-07-29 15:47:11 +0300

    fixes

--- encryption/sytmetric_encryption.py
+++ encryption/sytmetric_encryption.py
@@ -8,15 +8,19 @@ class SymetricEncryption(Encryption):
         self._key = "61C85192344CBE563222ED9B28719"
 
     def encrypt(self, data_to_encrypt):
-        return self._crypt_function(data_to_encrypt)
+        previous_byte_result = ord(data_to_encrypt[0]) ^ ord(self._key[0])
+        text = chr(previous_byte_result)
+        for data_byte, key_byte in zip(data_to_encrypt[1:], cycle(self._key[1:])):
+            previous_byte_result = (ord(data_byte) ^
+                                    ord(key_byte) ^
+                                    previous_byte_result)
+            text += chr(previous_byte_result)
+        return text
 
     def decrypt(self, data_to_decrypt):
-        return self._crypt_function(data_to_decrypt)
-
-    def _crypt_function(self, data):
-        previous_byte_result = data[0] ^ self._key[0]
-        text = previous_byte_result
-        for data_byte, key_byte in zip(data[1:], cycle(self._key[1:])):
-            previous_byte_result += data_byte ^ key_byte ^ previous_byte_result
-            text += previous_byte_result
+        text = chr(ord(data_to_decrypt[0]) ^ ord(self._key[0]))
+        previous_byte = data_to_decrypt[0]
+        for data_byte, key_byte in zip(data_to_decrypt[1:], cycle(self._key[1:])):
+            text += chr(ord(data_byte) ^ ord(key_byte) ^ ord(previous_byte))
+            previous_byte = data_byte
         return text
